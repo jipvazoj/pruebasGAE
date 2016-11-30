39910a1ae1572a49c66d87ce18473c7ae8035dd9
jipvazoj <jipvazoj@users.noreply.github.com>
2016-11-30 12:05:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hola Miercoles!')
+        self.response.write('Hola Miercoles JODIDO!')
 
 
 app = webapp2.WSGIApplication([
